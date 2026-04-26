8acad2af914f53bd8974c8f7ca0d6f7228d90c2b
Carwyn-G <gica25fx@student.ju.se>
2026-04-26 18:33:51 +0200

    Add: image carousels on services page

--- script.js
+++ script.js
@@ -20,3 +20,83 @@ if (document.getElementById("portfolio")!= null){
         document.getElementById(last).focus();
     }
 }
+
+function nextImg(a){
+    if (a === "location") {
+        let one = document.getElementsByClassName("loc-one");
+        let two = document.getElementsByClassName("loc-two");
+        let three = document.getElementsByClassName("loc-three");
+        console.log(one[0].id);
+        if (one[0].id === "loc-top") {
+            one[0].id = "loc-left";
+            two[0].id = "loc-top";
+            three[0].id = "loc-right";
+        } else if (one[0].id === "loc-left") {
+            one[0].id = "loc-right";
+            two[0].id = "loc-left";
+            three[0].id = "loc-top";
+        } else if (one[0].id === "loc-right") {
+            one[0].id = "loc-top";
+            two[0].id = "loc-right";
+            three[0].id = "loc-left";
+        } else {console.log("how did this happen?")}
+    } else if (a === "studio") {
+        let one = document.getElementsByClassName("stu-one");
+        let two = document.getElementsByClassName("stu-two");
+        let three = document.getElementsByClassName("stu-three");
+        console.log(one[0].id);
+        if (one[0].id === "stu-top") {
+            one[0].id = "stu-left";
+            two[0].id = "stu-top";
+            three[0].id = "stu-right";
+        } else if (one[0].id === "stu-left") {
+            one[0].id = "stu-right";
+            two[0].id = "stu-left";
+            three[0].id = "stu-top";
+        } else if (one[0].id === "stu-right") {
+            one[0].id = "stu-top";
+            two[0].id = "stu-right";
+            three[0].id = "stu-left";
+        } else {console.log("how did this happen?")}
+    } else {console.log ("how did you get here?")}
+}
+
+function prevImg(a){
+    if (a === "location") {
+        let one = document.getElementsByClassName("loc-one");
+        let two = document.getElementsByClassName("loc-two");
+        let three = document.getElementsByClassName("loc-three");
+        console.log(one[0].id);
+        if (one[0].id === "loc-top") {
+            one[0].id = "loc-right";
+            two[0].id = "loc-left";
+            three[0].id = "loc-top";
+        } else if (one[0].id === "loc-left") {
+            one[0].id = "loc-top";
+            two[0].id = "loc-right";
+            three[0].id = "loc-left";
+        } else if (one[0].id === "loc-right") {
+            one[0].id = "loc-left";
+            two[0].id = "loc-top";
+            three[0].id = "loc-right";
+        } else {console.log("how did this happen?")}
+    } else if (a === "studio") {
+        let one = document.getElementsByClassName("stu-one");
+        let two = document.getElementsByClassName("stu-two");
+        let three = document.getElementsByClassName("stu-three");
+        console.log(one[0].id);
+        if (one[0].id === "stu-top") {
+            one[0].id = "stu-right";
+            two[0].id = "stu-left";
+            three[0].id = "stu-top";
+        } else if (one[0].id === "stu-left") {
+            one[0].id = "stu-top";
+            two[0].id = "stu-right";
+            three[0].id = "stu-left";
+        } else if (one[0].id === "stu-right") {
+            one[0].id = "stu-left";
+            two[0].id = "stu-top";
+            three[0].id = "stu-right";
+        } else {console.log("how did this happen?")}
+    } else {console.log ("how did you get here?")}
+}
